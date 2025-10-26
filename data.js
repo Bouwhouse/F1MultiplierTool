b151efda30c3097946645a03341469a4473756f4
Bouwhouse <111579066+Bouwhouse@users.noreply.github.com>
2025-10-26 09:46:19 +0100

    Update data.js

--- data.js
+++ data.js
@@ -4,28 +4,29 @@
 const drivers = [
   { name: "Norris", class: "A" },
   { name: "Piastri", class: "A" },
+  { name: "Verstappen", class: "A" },
   
-  { name: "Verstappen", class: "B" },
   { name: "Russell", class: "B" },
   { name: "Leclerc", class: "B" },
   { name: "Hamilton", class: "B" },
   
   { name: "Antonelli", class: "C" },
-  { name: "Albon", class: "C" },
-  { name: "Sainz", class: "C" },
+  { name: "Hadjar", class: "C" },
   
-  { name: "Tsunoda", class: "D" },
-  { name: "Hadjar", class: "D" },
-  { name: "Ocon", class: "D" },
+  { name: "Sainz", class: "D" },
+  { name: "Hülkenberg", class: "D" },
   { name: "Alonso", class: "D" },
-  { name: "Lawson", class: "D" },
+  { name: "Albon", class: "D" },
   
+  { name: "Bearman", class: "D" },
+  { name: "Tsunoda", class: "D" },
+  
+  { name: "Ocon", class: "E" },
+  { name: "Lawson", class: "E" },
+  { name: "Bortoleto", class: "E" },
   { name: "Gasly", class: "E" },
-  { name: "Bearman", class: "E" },
-  { name: "Stroll", class: "E" },
-  { name: "Hülkenberg", class: "E" },
   { name: "Colapinto", class: "E" },
-  { name: "Bortoleto", class: "E" }
+  { name: "Stroll", class: "E" }
 ];
 
 // Puntentabel per klasse (positie 1 t/m 20)
@@ -36,4 +37,5 @@ const pointsTable = {
   D: [300, 280, 260, 240, 220, 200, 180, 160, 140, 120, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10],
   E: [340, 320, 300, 280, 260, 240, 220, 200, 180, 160, 140, 120, 100, 90, 80, 70, 60, 50, 40, 30]
 };
+
 
